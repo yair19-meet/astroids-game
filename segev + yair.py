2c9f0212566358450b055153a7323a99ed28774a
yair19-meet <yair19@meet.mit.edu>
2018-02-21 19:16:03 +0200

    aploaded

--- segev + yair.py
+++ segev + yair.py
@@ -1,5 +1,6 @@
 import turtle
-import random 
+import random
+import time
 turtle.tracer(1,0) 
 SIZE_X=900
 SIZE_Y=600
@@ -11,6 +12,8 @@ turtle.penup()
 SQUARE_SIZE = 50
 
 
+turtle.register_shape("Space_Shipp.gif")
+
 
 
 
@@ -19,13 +22,13 @@ START_LENGTH = 1
 
 space = turtle.clone()
 space.goto(0,-200)
-space.color("blue")
-space.shape("circle")
+space.shape("Space_Shipp.gif")
 
 turtle.hideturtle()
 
 
-
+arrow_circle = turtle.clone()
+arrow_circle.shape('circle')
 
 
 
@@ -54,13 +57,53 @@ arrow.setheading(90)
 arrow.penup()
 
 def move():
+    bool = False
     arrow.goto(space.xcor(), space.ycor())
+    arrow_circle.goto(arrow.xcor(), arrow.ycor())
     arrow.showturtle()
     while arrow.ycor() < SCREEN_HEIGHT - 10:
         current_y = arrow.ycor()
-        new_y = arrow.ycor() + 0.5
+        new_y = arrow.ycor() + 0.25
         arrow.goto(arrow.xcor(), new_y)
-    arrow.hideturtle()
+        arrow_circle.goto(arrow.xcor(), arrow.ycor())
+
+        if arrow.ycor() > SCREEN_HEIGHT - 60:
+            arrow.color("orange")
+
+        if arrow.ycor() > SCREEN_HEIGHT - 15:
+            bool = True
+
+
+#    Color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+ #   r = random.randrange(0, 257, 10)
+ #   g = random.randrange(0, 257, 10)
+ #   b = random.randrange(0, 257, 10)
+    
+    arrow.color("black")
+    if bool:
+     #   arrow.hideturtle()
+     #   turtle.bgcolor("orange")
+     #   turtle.forward(10000)
+    #    turtle.bgcolor("white")
+
+        arrow.hideturtle()
+
+        turtle.color("black", "orange")
+        arrow.penup()
+        turtle.penup()
+        turtle.begin_fill()
+        turtle.goto(arrow.xcor() - 10, SCREEN_HEIGHT)
+        turtle.goto(arrow.xcor() - 10, -700)
+        turtle.goto(arrow.xcor() + 15, -700)
+        turtle.goto(arrow.xcor() + 15, SCREEN_HEIGHT)
+        turtle.goto(arrow.xcor() - 10, SCREEN_HEIGHT)
+        turtle.end_fill()
+        turtle.forward(10000)
+        turtle.reset()
+        turtle.hideturtle()
+        
+
 
 
 
