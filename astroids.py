06682d17800c490914e4db0f1e53d32f6423f348
leen19-meet <leen19@meet.mit.edu>
2018-02-28 17:31:01 +0200

    Add files via upload

--- astroids.py
+++ astroids.py
@@ -1,11 +1,14 @@
 from turtle import Turtle
+import turtle
+import random
+turtle.tracer(0)
 class Asteroids(Turtle):
     def __init__(self,x,y,dy,r,color):
         Turtle.__init__(self)
         self.penup()
         self.goto(x, y)
         self.dy = dy
-	self.x = x
+        self.x = x
         self.r = r
         self.color(color)
         self.shape("circle")
@@ -18,17 +21,40 @@ class Asteroids(Turtle):
           self.goto(self.x,SCREEN_HEIGHT)
 
         self.goto(new_x,new_y)
+    def MoveDown():
+        # it moves the balls down while there is no collision between the balls and the arrow and the ground
+       # a = self.y-1
+       # self.y = a
+       # self.goto(self.x,a)
+       self.forward(-2)
 
-
+RUNNING = True
+      
+MAXIMUM_BALL_RADIUS = 20
+MINIMUM_BALL_RADIUS = 10
+MAXIMUM_BALL_Dy = 5
+MINIMUM_BALL_Dy = 3
 SCREEN_WIDTH = turtle.getcanvas().winfo_width()/2
 SCREEN_HEIGHT = turtle.getcanvas().winfo_height()/2
-ASTEROIDS[]
+ASTEROIDS=[]
 
-for i in range(NUMBER_OF_BALLS):
-     x = random.randint(int(-SCREEN_WIDTH) + MAXIMUM_BALL_RADIUS ,int(SCREEN_WIDTH)- MAXIMUM_BALL_RADIUS)
-     y = screen_height
-     dy =random.randint(MINIMUM_BALL_Dy, MAXIMUM_BALL_Dy)
-     radius =random.randint(MINIMUM_BALL_RADIUS, MAXIMUM_BALL_RADIUS)
-     color =(random.random(),random.random(),random.random())
-     New_Asteroids = Asteroids(x,y,dy,radius, color)
-     ASTEROIDS.append(new_asteroid) 
+def CREATEBALLS():
+    NUMBER_OF_BALLS= 10
+    for i in range(NUMBER_OF_BALLS):
+         x = random.randint(int(-SCREEN_WIDTH) + MAXIMUM_BALL_RADIUS ,int(SCREEN_WIDTH)- MAXIMUM_BALL_RADIUS)
+         y = SCREEN_HEIGHT
+         dy =random.randint(MINIMUM_BALL_Dy, MAXIMUM_BALL_Dy)
+         radius =random.randint(MINIMUM_BALL_RADIUS, MAXIMUM_BALL_RADIUS)
+         color =(random.random(),random.random(),random.random())
+         New_Asteroids = Asteroids(x,y,dy,radius, color)
+         ASTEROIDS.append(New_Asteroids)
+         turtle.update()
+         
+CREATEBALLS()
+
+#ASTEROIDS[0].MoveDown()
+while RUNNING == True:
+    for i in ASTEROIDS:
+       i.MoveDown()
+
+
